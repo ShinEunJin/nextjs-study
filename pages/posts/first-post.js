4bcea3c067e5309fe772e65e56e663762870d07c
ShinEunJin <sineun5501@naver.com>
2022-02-06 23:23:12 +0900

    Image

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,22 @@
 import Link from "next/link"
+import Image from "next/image"
 
 const FirstPost = () => {
   return (
-    <h1>
-      <Link href="/">
-        <a>first post</a>
-      </Link>
-    </h1>
+    <>
+      <h1>
+        <Link href="/">
+          <a>first post</a>
+        </Link>
+      </h1>
+      <Image
+        src={"/images/test.jpg"}
+        width={400}
+        height={400}
+        alt="df"
+        objectFit="contain"
+      />
+    </>
   )
 }
 
